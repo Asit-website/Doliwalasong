8e548459b57ffbc2a98907349f500c6bb6cf4ac5
Asit-website <asitmandal492@gmail.com>
2022-09-04 11:54:00 +0530

    all changes done

---
+++ js/footer.js
@@ -0,0 +1,82 @@
+document.write(`<footer>
+<div class="aboutUs">
+  <span>About Us</span>
+  <div class="about_para">
+    <p>Solution for all weddings and corporate</p>
+    <p>events management and entertainment. Shahi</p>
+    <p>पालकी Palki & Shahi डोली Doli for Bride’s दुल्हन Entry &</p>
+    <p>Vidaai with attractive themes.</p>
+  </div>
+  <div class="about_image">
+    <a target="_blank" href="https://instagram.com/doliwala_events_and_wedding?igshid=YmMyMTA2M2Y="><img src="images/Mask Group 12.png" alt="ja"></a>
+    <a target="_blank" href="https://www.facebook.com/doliwalaevents/"><img src="images/Mask Group 6.png" alt="no"></a>
+    <a target="-blank" href="https://twitter.com/doliwalaevents?t=h0mVouDjALrc0QEeGZ2r7Q&s=09"><img src="images/Mask Group 8.png" alt="it"></a>
+    <a target="-blank" href="https://play.google.com/store/apps/details?id=com.koo.app&referrer=adjust_reftag%3DcNPtdRri1ueyH%26utm_source%3DKoo_Website%26utm_campaign%3Dshare_dnld_link"><img src="images/Mask Group 10.png" alt="so"></a>
+    <a target="-blank" href="https://youtube.com/channel/UCqMQTrx_gCy0WSjfStYIVeA"><img src="images/Mask Group 11.png" alt="be"></a>
+  </div>
+</div>
+<div class="Information">
+  <span>Information</span>
+  <div class="anchor_information">
+    <p><a href="index.html">Home</a></p>
+    <p><a href="about.html">About Us</a></p>
+    <p><a href="weddingPlaner.html">Wedding Planner</a></p>
+    <p><a href="Gallary.html">Gallery</a></p>
+    <p><a href="services.html">Services</a></p>
+    <p><a href="contact.html">Contact Us</a></p>
+  </div>
+</div>
+
+<div class="contact">
+  <span>Contact</span>
+  <div class="contact_para">
+    <p>If you have any queries, <br/> please feel free to <br/> reach out to us.
+    </p>
+    
+  </div>
+  <div class="doliWala_textImg" style="display: flex; flex-direction:column;">
+    <div class="doliwala_try" style="display: flex ; margin-top: 18px;">
+      <img style="width:17px ; height:17px;" src="images/Mask Group 15.png" alt="">
+      <p class="dir">Doliwala.india@gmail.com</p>
+    </div>
+    <div class="doliwala_try" style="display:flex ;margin-top: 18px;">
+      <img style="width:17px ; height:17px;" src="images/Mask Group 5.png" alt="">
+      <p class="dir">+91 9015608586, +91 9873665082</p>
+    </div>
+
+    <div class="doliwala_try" style="display: flex; margin-top: 18px;">
+      <img style="width:17px ; height:17px;" src="images/Mask Group 13.png" alt="">
+      <div class="dir">
+        <p>407 Namdhari Chamber</p>
+        <p>Karol Bagh</p>
+        <p>New Delhi 110005</p>
+      </div>
+
+    </div>
+  </div>
+
+</div>
+
+
+<div class="get_in">
+  <span style="text-transform: capitalize;">Get In Touch with us</span>
+  <img src="images/get-it-on-google-play-badge.png" alt="">
+</div>
+</footer>
+
+
+
+<div  class="another_footer">
+<p >
+  © Copyright All Right Reserved M/S Doliwala डोलीवाला Events LLP ® Registered Trademark
+</p>
+<a href="www.kusheldigi.com" target="_blank"><p >Designed & Developed By Kushel Digi Solutions</p></a>
+</div>
+`);
+
+{/* <p>Would you any</p>
+    <p>enquires. please feel</p>
+    <p>Free to contact us</p> */}
+
+
+
---
+++ js/index.js
@@ -0,0 +1,43 @@
+
+let meg = document.querySelector("#meg");
+let template = document.createElement('template');
+template.innerHTML = `<div><header class="head">
+<div class="first_head">
+   <div class="first_span">
+      <span><img src="images/Mask Group 22.png" alt="">Mr. Parvindar(परविन्दर)</span>
+   </div>
+   <div class="second_span">
+      <span><img src="images/Mask Group 5.png" alt="">+91 9015608586</span>
+   </div>
+</div>
+</header>
+<nav class="navbar second">
+    <div class="first_nav">
+       <img src="images/icon.png" alt="nothing">
+    </div>
+    <div class="second_nav">
+      <button style=" padding: 5px 20px; background-color: hotpink; border: none; outline: none; border-radius: 5px; color: #FFFFFF;" class="burger">Menu</button>
+      <!-- <p style="color: hotpink; font-weight:bold; text-transform: capitalize; padding: 0; margin: 0;" id="men">menu</p> -->
+       <ul class="fing v-class-resp">
+          <li ><a  href="index.html">Home</a></li>
+          <li ><a  href="about.html">About Us</a></li>
+          <li ><a  href="weddingPlaner.html">Wedding Planner</a></li>
+          <li ><a  href="services.html">Services</a></li>
+          <li ><a  href="Gallary.html">Gallery</a></li>
+          <li ><a href="contact.html">Contact Us</a></li>
+       </ul>
+    </div>
+ </nav>
+</div>
+`;
+ meg.appendChild(template.content);
+
+ 
+
+//  <div class="second_head">
+//    <a target="_blank" href="https://instagram.com/doliwala_events_and_wedding?igshid=YmMyMTA2M2Y="><img src="images/Mask Group 12.png" alt="ja"></a>
+//    <a target="_blank" href="https://www.facebook.com/doliwalaevents/"><img src="images/Mask Group 6.png" alt="no"></a>
+//    <a target="-blank" href="https://twitter.com/doliwalaevents?t=h0mVouDjALrc0QEeGZ2r7Q&s=09"><img src="images/Mask Group 8.png" alt="it"></a>
+//    <a target="-blank" href="https://play.google.com/store/apps/details?id=com.koo.app&referrer=adjust_reftag%3DcNPtdRri1ueyH%26utm_source%3DKoo_Website%26utm_campaign%3Dshare_dnld_link"><img src="images/Mask Group 10.png" alt="so"></a>
+//    <a target="-blank" href="https://youtube.com/channel/UCqMQTrx_gCy0WSjfStYIVeA"><img src="images/Mask Group 11.png" alt="be"></a>
+// </div>
