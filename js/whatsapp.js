484a87c8cb376b7afc66e95dcfefe8bdffe6e8ce
Asit-website <asitmandal492@gmail.com>
2022-09-18 00:21:17 +0530

    this

---
+++ js/whatsapp.js
@@ -0,0 +1,4 @@
+console.log("welcome to whatsapp chat");
+document.write(` <a target="_blank" href="https://api.whatsapp.com/send/?phone=+919015608586&amp;text= hi" class="whatsapp">
+<i  class="fa-brands fa-whatsapp my-float"></i>
+</a>`);
